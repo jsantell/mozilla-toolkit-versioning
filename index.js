a1251310d119e99d167c6041f45cb20b1d78225f
asamuzaK (Kazz) <kazz@asamuzak.jp>
2016-02-08 00:12:31 +0900

    Rewrite index.js

--- index.js
+++ index.js
@@ -1,69 +1,98 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-var versionParse = require('./lib/utils').versionParse;
 
-var COMPARATORS = ['>=', '<=', '>', '<', '=', '~', '^'];
+exports.parse = function (input) {
+  var ERROR_MESSAGE = '`parse` argument must be a populated string.';
 
-exports.parse = function (input) { 
+  // Expression to match Mozilla's Toolkit Format.
+  // https://developer.mozilla.org/en-US/docs/Toolkit_version_format
+  var VERSION_PART = '(?:(?:-?[\d]+)?(?:[!-\-\/:-~]+)?){2}';
+  var VERSION_FORMAT = `(?:${VERSION_PART})?(?:\.(?:${VERSION_PART})?)*`;
+  var COMPARATOR = '[><]=?';
+  var VERSION_STRING = `(${COMPARATOR})?(${VERSION_FORMAT})`;
+
   input = input || '';
   input = input.trim();
-  if (!input)
-    throw new Error('`parse` argument must be a populated string.');
 
-  // Handle the "*" case
-  if (input === "*") {
+  var inputs = input.split(/\s+/);
+
+  if (!input ||
+      !(new RegExp(`${VERSION_STRING}`)).test(input) ||
+      inputs.length < 1 ||
+      inputs.length > 3) {
+    throw new Error(ERROR_MESSAGE);
+  }
+
+  // Handle the '*' case
+  // note: Maybe return value { min: undefined, max: '*' } should be better?
+  if (input === '*') {
     return { min: undefined, max: undefined };
   }
 
-  var inputs = input.split(' ');
-  var min;
-  var max;
+  var min, max;
 
   // 1.2.3 - 2.3.4
-  if (inputs.length === 3 && inputs[1] === '-') {
-    return { min: inputs[0], max: inputs[2] };
+  if (inputs.length === 3) {
+    // note: If '>=1.2.3 - <=2.3.4' is acceptable (version string with COMPARATORs),
+    // then expression for version string should be
+    // new RegExp(`${VERSION_STRING}`)
+    var exp = new RegExp(`^${VERSION_FORMAT}$`);
+    if (inputs[1] === '-' && exp.test(inputs[0]) && exp.test(inputs[2])) {
+      return { min: inputs[0], max: inputs[2] };
+    }
+    else {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
+  else {
+    // inputs.length will be 1 or 2
+    for (var i = 0, l = inputs.length; i < l; i++) {
+      var str = (new RegExp(`^${VERSION_STRING}$`)).exec(inputs[i]);
+      if (str) {
+        if (str[1]) {
+          switch (str[1]) {
+            case '<':
+              max = decrement(str[2]);
+              break;
+            case '<=':
+              max = str[2];
+              break;
+            case '>':
+              min = increment(str[2]);
+              break;
+            case '>=':
+              min = str[2];
+              break;
+            default:
+              if (i === 0) {
+                min = str[2];
+              }
+              else {
+                max = str[2];
+              }
+          }
+        }
+        else {
+          if (i === 0) {
+            min = max = str[2];
+            if (l === 1) {
+              break;
+            }
+          }
+          else {
+            max = str[2];
+          }
+        }
+      }
+      else {
+        throw new Error(ERROR_MESSAGE);
+      }
+    }
   }
 
-  inputs.forEach(function (input) {
-    var parsed = parseExpression(input);
-    var version = parsed.version;
-    var comparator = parsed.comparator;
-
-    // 1.2.3
-    if (inputs.length === 1 && !comparator)
-      min = max = version;
-
-    // Parse min
-    if (~comparator.indexOf('>')) {
-      if (~comparator.indexOf('='))
-        min = version; // >=1.2.3
-      else
-        min = increment(version); // >1.2.3
-    }
-    else if (~comparator.indexOf('<')) {
-      if (~comparator.indexOf('='))
-        max = version; // <=1.2.3
-      else
-        max = decrement(version); // <1.2.3
-    }
-  });
-
-  return {
-    min: min,
-    max : max
-  };
+  return { min: min, max: max };
 };
-
-function parseExpression (input) {
-  for (var i = 0; i < COMPARATORS.length; i++)
-    if (~input.indexOf(COMPARATORS[i]))
-      return { 
-        comparator: COMPARATORS[i],
-        version: input.substr(COMPARATORS[i].length)
-      };
-  return { version: input, comparator: '' };
-}
 
 /**
  * Takes a version string ('1.2.3') and returns a version string
@@ -73,6 +102,7 @@ function parseExpression (input) {
  * @return {String}
  */
 function decrement (vString) {
+  // note: Always add '.' and return `${vString}.-1`; looks better.
   return vString + (vString.charAt(vString.length - 1) === '.' ? '' : '.') + '-1';
 }
 exports.decrement = decrement;
@@ -81,31 +111,12 @@ exports.decrement = decrement;
  * Takes a version string ('1.2.3') and returns a version string
  * that'll parse as greater than the input string by the smallest margin
  * possible ('1.2.3.1').
- * listed as number-A, string-B, number-C, string-D in 
- * Mozilla's Toolkit Format.
- * https://developer.mozilla.org/en-US/docs/Toolkit_version_format
  *
  * @param {String} vString
  * @return {String}
  */
 function increment (vString) {
-  var match = versionParse(vString);
-  var a = match[1];
-  var b = match[2];
-  var c = match[3];
-  var d = match[4];
-  var lastPos = vString.length - 1;
-  var lastChar = vString.charAt(lastPos);
-
-  if (!b) {
-    return vString + (lastChar === '.' ? '' : '.') + '1';
-  }
-  if (!c) {
-    return vString + '1';
-  }
-  if (!d) {
-    return vString.substr(0, lastPos) + (++lastChar);
-  }
-  return vString.substr(0, lastPos) + String.fromCharCode(lastChar.charCodeAt(0) + 1);
+  // note: Always add '.' and return `${vString}.1`; looks better.
+  return vString + (vString.charAt(vString.length - 1) === '.' ? '' : '.') + '1';
 }
 exports.increment = increment;
